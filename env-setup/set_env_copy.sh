333699e13f5a99da80eb3189b4152143dd8ca41e
tutikashilpa <tutikashilpa@gmail.com>
2020-06-20 12:44:26 -0700

    test

--- env-setup/set_env_copy.sh
+++ env-setup/set_env_copy.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 export TEST='test'
 export GCP_PROJECT_ID=$(gcloud config list --format 'value(core.project)')
 export PROJECT_NUMBER=$(gcloud projects describe "${GCP_PROJECT_ID}" --format='get(projectNumber)')
@@ -8,15 +7,9 @@ export INPUT_BUCKET_TEST="${GCP_PROJECT_ID}-composer-input-${TEST}"
 export RESULT_BUCKET_TEST="${GCP_PROJECT_ID}-composer-result-${TEST}"
 export REF_BUCKET_TEST="${GCP_PROJECT_ID}-composer-ref-${TEST}"
 export DATAFLOW_STAGING_BUCKET_TEST="${GCP_PROJECT_ID}-dataflow-staging-${TEST}"
-export PROD='prod'
-export DATAFLOW_JAR_BUCKET_PROD="${GCP_PROJECT_ID}-composer-dataflow-source-${PROD}"
-export INPUT_BUCKET_PROD="${GCP_PROJECT_ID}-composer-input-${PROD}"
-export RESULT_BUCKET_PROD="${GCP_PROJECT_ID}-composer-result-${PROD}"
-export DATAFLOW_STAGING_BUCKET_PROD="${GCP_PROJECT_ID}-dataflow-staging-${PROD}"
 export COMPOSER_REGION='us-central1'
 export RESULT_BUCKET_REGION="${COMPOSER_REGION}"
 export COMPOSER_ZONE_ID='us-central1-a'
-
 export COMPOSER_ENV_NAME='data-pipeline-composer'
 export SOURCE_CODE_REPO='data-pipeline-source'
 export COMPOSER_DAG_NAME_TEST='test_word_count'
